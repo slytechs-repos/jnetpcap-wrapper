4aaf2dcf330f8d72979be5b48c2a85ddcdd1a1f1
Mark Bednarczyk <mark@slytechs.com>
2023-10-19 16:58:08 -0400

    Changed PcapMessages resource bundle to use ResourceBundleProvider for modular I8N integration

---
+++ src/main/java/org/jnetpcap/Messages.java
@@ -0,0 +1,43 @@
+/*
+ * Sly Technologies Free License
+ * 
+ * Copyright 2023 Sly Technologies Inc.
+ *
+ * Licensed under the Sly Technologies Free License (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.slytechs.com/free-license-text
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.jnetpcap;
+
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+/**
+ * @author Sly Technologies Inc
+ * @author repos@slytechs.com
+ *
+ */
+public class Messages {
+	private static final String BUNDLE_NAME = Messages.class.getPackageName() + ".messages"; //$NON-NLS-1$
+
+	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
+
+	private Messages() {
+	}
+
+	public static String getString(String key) {
+		try {
+			return RESOURCE_BUNDLE.getString(key);
+		} catch (MissingResourceException e) {
+			return '!' + key + '!';
+		}
+	}
+}
---
+++ src/main/java/org/jnetpcap/spi/PcapMessagesProvider.java
@@ -0,0 +1,29 @@
+/*
+ * Sly Technologies Free License
+ * 
+ * Copyright 2023 Sly Technologies Inc.
+ *
+ * Licensed under the Sly Technologies Free License (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.slytechs.com/free-license-text
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.jnetpcap.spi;
+
+import java.util.spi.ResourceBundleProvider;
+
+/**
+ * @author Sly Technologies Inc
+ * @author repos@slytechs.com
+ *
+ */
+public interface PcapMessagesProvider extends ResourceBundleProvider {
+
+}
